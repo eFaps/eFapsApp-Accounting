107fe6ed5cd9dde0efa44264c9d255a75a6f6fcd
jan@moxter.net <jan@moxter.net@fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0>
2012-12-07 18:13:05 +0000

    - accounting: adding Documents to SalesRecord
    git-svn-id: https://efaps.googlecode.com/svn/eFapsApps/accounting/trunk@8317 fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0

---
+++ src/main/efaps/ESJP/org/efaps/esjp/accounting/SalesRecord_Base.java
@@ -0,0 +1,62 @@
+package org.efaps.esjp.accounting;
+
+import org.efaps.admin.event.Parameter;
+import org.efaps.admin.event.Return;
+import org.efaps.admin.program.esjp.EFapsRevision;
+import org.efaps.admin.program.esjp.EFapsUUID;
+import org.efaps.db.AttributeQuery;
+import org.efaps.db.QueryBuilder;
+import org.efaps.esjp.ci.CIAccounting;
+import org.efaps.esjp.ci.CIERP;
+import org.efaps.esjp.common.uitable.MultiPrint;
+import org.efaps.util.EFapsException;
+
+/*
+ * Copyright 2003 - 2012 The eFaps Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Revision:        $Rev$
+ * Last Changed:    $Date$
+ * Last Changed By: $Author$
+ */
+
+/**
+ * TODO description!
+ *
+ * @author The eFasp Team
+ * @version $Id$
+ */
+@EFapsUUID("2198d008-b65f-4d3c-b84d-48250c047708")
+@EFapsRevision("$Rev$")
+public abstract class SalesRecord_Base
+{
+
+    public Return documentMultiPrint(final Parameter _parameter)
+        throws EFapsException
+    {
+        final MultiPrint multi = new MultiPrint() {
+            @Override
+            protected void add2QueryBldr(final Parameter _parameter,
+                                         final QueryBuilder _queryBldr)
+                throws EFapsException
+            {
+                final QueryBuilder attrQueryBldr = new QueryBuilder(CIAccounting.SalesRecord2Document);
+                final AttributeQuery attrQuery = attrQueryBldr.getAttributeQuery(
+                                CIAccounting.SalesRecord2Document.ToLink);
+                _queryBldr.addWhereAttrNotInQuery(CIERP.DocumentAbstract.ID, attrQuery);
+            }
+        };
+        return multi.execute(_parameter);
+    }
+}
