fb9296d163f1e3c01fc196f57fdd23d9966d8fcb
rosa.ordonez@efaps.org <rosa.ordonez@efaps.org@fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0>
2012-12-14 00:26:11 +0000

    - accounting: add a trigger to link Proof type to PurchaseRecord2Document after to connect
    git-svn-id: https://efaps.googlecode.com/svn/eFapsApps/accounting/trunk@8384 fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0

--- src/main/efaps/ESJP/org/efaps/esjp/accounting/PurchaseRecord_Base.java
+++ src/main/efaps/ESJP/org/efaps/esjp/accounting/PurchaseRecord_Base.java
@@ -1,17 +1,37 @@
 package org.efaps.esjp.accounting;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+import java.util.UUID;
+
+import org.apache.ecs.xhtml.select;
 import org.efaps.admin.access.AccessSet;
+import org.efaps.admin.common.SystemConfiguration;
 import org.efaps.admin.event.Parameter;
 import org.efaps.admin.event.Return;
+import org.efaps.admin.event.Parameter.ParameterValues;
 import org.efaps.admin.program.esjp.EFapsRevision;
 import org.efaps.admin.program.esjp.EFapsUUID;
 import org.efaps.db.AttributeQuery;
+import org.efaps.db.Context;
+import org.efaps.db.Insert;
 import org.efaps.db.Instance;
+import org.efaps.db.MultiPrintQuery;
+import org.efaps.db.PrintQuery;
 import org.efaps.db.QueryBuilder;
+import org.efaps.db.Context.FileParameter;
+import org.efaps.db.SelectBuilder;
+import org.efaps.db.Update;
+import org.efaps.esjp.accounting.Import_Base.ImportAccount;
+import org.efaps.esjp.accounting.transaction.Transaction_Base;
+import org.efaps.esjp.admin.common.SystemConf;
 import org.efaps.esjp.ci.CIAccounting;
 import org.efaps.esjp.ci.CIERP;
 import org.efaps.esjp.ci.CIFormAccounting;
+import org.efaps.esjp.ci.CISales;
 import org.efaps.esjp.common.uitable.MultiPrint;
+import org.efaps.update.schema.common.SystemConfigurationUpdate;
 import org.efaps.util.EFapsException;
 import org.jfree.util.Log;
 import org.slf4j.Logger;
@@ -47,7 +67,6 @@ import org.slf4j.LoggerFactory;
 @EFapsRevision("$Rev$")
 public abstract class PurchaseRecord_Base
 {
-    private static final Logger LOG = LoggerFactory.getLogger(PurchaseRecord_Base.class);
     
     public Return documentMultiPrint(final Parameter _parameter)
         throws EFapsException
@@ -69,11 +88,33 @@ public abstract class PurchaseRecord_Base
         return multi.execute(_parameter);
     }
 
-    public Return updateLinkTrigger(final Parameter _parameter)
+    public Return insertPostTrigger(final Parameter _parameter)
         throws EFapsException
     {
-        Instance instance = _parameter.getInstance(); // hasta aqui tiene Accounting_PurchaseRecord2Document
-        
+        Instance instance = _parameter.getInstance();
+        PrintQuery print = new PrintQuery(instance);
+        SelectBuilder selectDoc = new SelectBuilder().linkto(CIAccounting.PurchaseRecord2Document.ToLink).oid();
+        print.addSelect(selectDoc);
+        print.execute();
+        String docOid = print.<String>getSelect(selectDoc);
+        Instance docInst = Instance.get(docOid);
+        if(docInst.isValid()){
+            if(docInst.getType().isKindOf(CISales.IncomingInvoice.getType())){
+                //Acccounting Configuration
+                SystemConfiguration config = SystemConfiguration.get(
+                                UUID.fromString("ca0a1df1-2211-45d9-97c8-07af6636a9b9"));
+                
+                if (config != null) {
+                    Instance link = config.getLink("externalLink");
+                    if (link.isValid()) {
+                    Update update = new Update(instance);
+                    update.add(CIAccounting.PurchaseRecord2Document.TypeLink, link.getId());
+                    update.executeWithoutTrigger();
+                    }
+                }
+            }
+        }
+
         return new Return();
     }
 }
