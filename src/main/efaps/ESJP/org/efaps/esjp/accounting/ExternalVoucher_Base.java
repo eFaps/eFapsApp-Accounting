7c4b94e036e7a2b5977933ce8db45d3fca9cb8ef
m.aranya@moxter.net <m.aranya@moxter.net@fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0>
2013-06-19 17:36:00 +0000

    - accounting : remove method to obtains payments schedule for type and update user interface using method multiprint.
    git-svn-id: https://efaps.googlecode.com/svn/eFapsApps/accounting/trunk@9655 fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0

--- src/main/efaps/ESJP/org/efaps/esjp/accounting/ExternalVoucher_Base.java
+++ src/main/efaps/ESJP/org/efaps/esjp/accounting/ExternalVoucher_Base.java
@@ -319,32 +319,4 @@ public abstract class ExternalVoucher_Base
         };
         return contactsPicker.picker4NewContact(_parameter);
     }
-
-    public Return getPaymentScheduleDocuments(final Parameter _parameter)
-        throws EFapsException
-    {
-        final Map<?, ?> properties = (Map<?, ?>) _parameter.get(ParameterValues.PROPERTIES);
-        final String typesStr = (String) properties.get("Types");
-        final String[] typesArray = typesStr.split(";");
-        final String statusStr = (String) properties.get("Status");
-        final String[] statusArray = statusStr.split(";");
-
-        final List<Instance> instances = new ArrayList<Instance>();
-        int cont = 0;
-        for (final String typeStr : typesArray) {
-            final Type type = Type.get(typeStr);
-            final QueryBuilder queryBldr = new QueryBuilder(type);
-            queryBldr.addWhereAttrEqValue(CISales.DocumentAbstract.StatusAbstract,
-                            Status.find(type.getStatusAttribute().getLink().getUUID(), statusArray[cont]).getId());
-            final InstanceQuery query = queryBldr.getQuery();
-            query.execute();
-            instances.addAll(query.getValues());
-            cont++;
-        }
-
-        final Return ret = new Return();
-        ret.put(ReturnValues.VALUES, instances);
-
-        return ret;
-    }
 }
