ba36cbc4e74418f361ce19b1470760053204aac8
jan@moxter.net <jan@moxter.net@fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0>
2013-08-29 02:11:24 +0000

    - accounting: adding data import
    git-svn-id: https://efaps.googlecode.com/svn/eFapsApps/accounting/trunk@10148 fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0

--- src/main/efaps/ESJP/org/efaps/esjp/accounting/util/data/ConSis.java
+++ src/main/efaps/ESJP/org/efaps/esjp/accounting/util/data/ConSis.java
@@ -24,22 +24,36 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.text.NumberFormat;
+import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+import org.efaps.admin.datamodel.Status;
 import org.efaps.admin.event.Parameter;
 import org.efaps.admin.event.Return;
 import org.efaps.admin.program.esjp.EFapsRevision;
 import org.efaps.admin.program.esjp.EFapsUUID;
+import org.efaps.db.Insert;
+import org.efaps.db.Instance;
 import org.efaps.db.InstanceQuery;
 import org.efaps.db.QueryBuilder;
 import org.efaps.esjp.ci.CIAccounting;
+import org.efaps.esjp.sales.util.Sales;
+import org.efaps.esjp.sales.util.SalesSettings;
 import org.efaps.util.EFapsException;
+import org.joda.time.DateTime;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import au.com.bytecode.opencsv.CSVReader;
 
 
 
@@ -85,9 +99,74 @@ public class ConSis
     }
 
     /**
+     * Needs a txt file from Concar. Reads it, analyses it and checks if an account with the same name exists.
+     * @param _parameter
+     * @return
+     */
+    public Return initialTransaction(final Parameter _parameter)
+    {
+        final String filename = _parameter.getParameterValue("valueField");
+        final File file = new File(filename);
+        try {
+            final List<Account> accs = readAccounts4SaldoConcar(file);
+            BigDecimal amountMN = BigDecimal.ZERO;
+            BigDecimal amountME = BigDecimal.ZERO;
+            boolean found = true;
+            for (final Account acc  :accs) {
+                final QueryBuilder queryBldr = new QueryBuilder(CIAccounting.AccountAbstract);
+                queryBldr.addWhereAttrEqValue(CIAccounting.AccountAbstract.Name, acc.getName());
+                final InstanceQuery query = queryBldr.getQuery();
+                query.executeWithoutAccessCheck();
+                if (query.next()) {
+                    acc.setInstance(query.getCurrentValue());
+                } else {
+                    ConSis.LOG.info("Not found: {}", acc);
+                    found  = false;
+                }
+                amountMN = amountMN.add(acc.getAmountMN());
+                amountME = amountME.add(acc.getAmountME());
+            }
+
+            if (found && amountMN.compareTo(BigDecimal.ZERO) == 0 && amountME.compareTo(BigDecimal.ZERO) == 0) {
+                final Insert insert = new Insert(CIAccounting.TransactionOpeningBalance);
+                insert.add(CIAccounting.TransactionOpeningBalance.Date, new DateTime());
+                insert.add(CIAccounting.TransactionOpeningBalance.Description, "Asiento de Abertura");
+                insert.add(CIAccounting.TransactionOpeningBalance.Status,
+                                Status.find(CIAccounting.TransactionStatus.Open));
+                insert.add(CIAccounting.TransactionOpeningBalance.PeriodeLink, _parameter.getInstance());
+                insert.executeWithoutAccessCheck();
+
+                final Instance basCur = Sales.getSysConfig().getLink(SalesSettings.CURRENCYBASE);
+                for (final Account acc : accs) {
+                    final Insert insertpos = new Insert(
+                                    acc.getAmountME().compareTo(BigDecimal.ZERO) > 0
+                                    ? CIAccounting.TransactionPositionCredit : CIAccounting.TransactionPositionDebit);
+                    insertpos.add(CIAccounting.TransactionPositionAbstract.AccountLink, acc.getInstance());
+                    insertpos.add(CIAccounting.TransactionPositionAbstract.Amount, acc.getAmountMN());
+                    insertpos.add(CIAccounting.TransactionPositionAbstract.CurrencyLink, basCur);
+                    insertpos.add(CIAccounting.TransactionPositionAbstract.Rate, acc.getRateObject());
+                    insertpos.add(CIAccounting.TransactionPositionAbstract.RateAmount, acc.getAmountME());
+                    insertpos.add(CIAccounting.TransactionPositionAbstract.RateCurrencyLink, 1);
+                    insertpos.add(CIAccounting.TransactionPositionAbstract.TransactionLink,  insert.getInstance());
+                    insertpos.executeWithoutAccessCheck();
+                }
+            }
+        } catch (final IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (final EFapsException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (final ParseException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return new Return();
+    }
+
+
+    /**
      * Read "P L A N   G E N E R A L   D E   C U E N T A S" from a txt. file/
-     *
-     *
      */
     protected List<Account> readAccounts4Concar(final File _file)
         throws IOException
@@ -115,11 +194,58 @@ public class ConSis
     }
 
 
+    protected List<Account> readAccounts4SaldoConcar(final File _file)
+        throws IOException, ParseException
+    {
+        final List<Account> ret = new ArrayList<Account>();
+        final BufferedReader in = new BufferedReader(new FileReader(_file));
+        final CSVReader reader = new CSVReader(in);
+        final List<String[]> rows = reader.readAll();
+        reader.close();
+        final Pattern accPattern = Pattern.compile("^[\\d ]*");
+        final DecimalFormat formater = (DecimalFormat) NumberFormat.getInstance();
+        formater.setParseBigDecimal(true);
+        final DecimalFormatSymbols frmSym = DecimalFormatSymbols.getInstance();
+        frmSym.setDecimalSeparator(",".toCharArray()[0]);
+        frmSym.setPatternSeparator(".".toCharArray()[0]);
+        formater.setDecimalFormatSymbols(frmSym);
+
+        for (final String[] row : rows) {
+            final String accStr = row[0];
+            if (!accStr.isEmpty() && accPattern.matcher(accStr).matches()) {
+                final Account acc = new Account();
+                ret.add(acc);
+                acc.setName(accStr.trim());
+                final String amountMEDebit = row[10];
+                final String amountMECredit = row[11];
+                final String amountMNDebit = row[12];
+                final String amountMNCredit = row[13];
+                if (!amountMEDebit.isEmpty()) {
+                    acc.setAmountME(((BigDecimal) formater.parse(amountMEDebit)).negate());
+                }
+                if (!amountMECredit.isEmpty()) {
+                    acc.setAmountME((BigDecimal) formater.parse(amountMECredit));
+                }
+                if (!amountMNDebit.isEmpty()) {
+                    acc.setAmountMN(((BigDecimal) formater.parse(amountMNDebit)).negate());
+                }
+                if (!amountMNCredit.isEmpty()) {
+                    acc.setAmountMN((BigDecimal) formater.parse(amountMNCredit));
+                }
+                ConSis.LOG.info("Read Account {} - Rate: {}", acc, acc.getRate());
+            }
+        }
+        return ret;
+    }
+
+
     public static class Account
     {
         private String name;
         private String description;
-
+        private BigDecimal amountMN = BigDecimal.ZERO;
+        private BigDecimal amountME = BigDecimal.ZERO;
+        private Instance instance;
         /**
          * Getter method for the instance variable {@link #name}.
          *
@@ -128,6 +254,28 @@ public class ConSis
         public String getName()
         {
             return this.name;
+        }
+
+        /**
+         * @return
+         */
+        public BigDecimal getRate()
+        {
+            return this.amountME.compareTo(BigDecimal.ZERO) == 0 ? BigDecimal.ZERO : this.amountMN.setScale(8).divide(
+                            this.amountME, BigDecimal.ROUND_HALF_UP);
+        }
+
+        public Object[] getRateObject()
+        {
+            return new Object[] {BigDecimal.ONE, getRate()};
+        }
+
+        /**
+         * @param _currentValue
+         */
+        public void setInstance(final Instance _instance)
+        {
+            this.instance = _instance;
         }
 
         /**
@@ -160,10 +308,63 @@ public class ConSis
             this.description = _description;
         }
 
+        /**
+         * Getter method for the instance variable {@link #amountMN}.
+         *
+         * @return value of instance variable {@link #amountMN}
+         */
+        public BigDecimal getAmountMN()
+        {
+            return this.amountMN;
+        }
+
+
+        /**
+         * Setter method for instance variable {@link #amountMN}.
+         *
+         * @param _amountMN value for instance variable {@link #amountMN}
+         */
+        public void setAmountMN(final BigDecimal _amountMN)
+        {
+            this.amountMN = _amountMN;
+        }
+
+
+        /**
+         * Getter method for the instance variable {@link #amountME}.
+         *
+         * @return value of instance variable {@link #amountME}
+         */
+        public BigDecimal getAmountME()
+        {
+            return this.amountME;
+        }
+
+
+        /**
+         * Setter method for instance variable {@link #amountME}.
+         *
+         * @param _amountME value for instance variable {@link #amountME}
+         */
+        public void setAmountME(final BigDecimal _amountME)
+        {
+            this.amountME = _amountME;
+        }
+
+        /**
+         * Getter method for the instance variable {@link #instance}.
+         *
+         * @return value of instance variable {@link #instance}
+         */
+        public Instance getInstance()
+        {
+            return this.instance;
+        }
+
         @Override
         public String toString()
         {
-            return ToStringBuilder.reflectionToString(this);
+            return ToStringBuilder.reflectionToString(this, ToStringStyle.SHORT_PREFIX_STYLE);
         }
 
     }
