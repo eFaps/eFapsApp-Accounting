b6a968150dcd5bba35465337f766a887a2ca462e
Jan Moxter <jan.moxter@innobix.com>
2017-11-14 23:15:32 -0500

    - Issue eFaps/eFaps-Kernel-Install#139: Rename method in AbstractDynamicReport

--- src/main/efaps/ESJP/org/efaps/esjp/accounting/report/DocumentDetailsReport_Base.java
+++ src/main/efaps/ESJP/org/efaps/esjp/accounting/report/DocumentDetailsReport_Base.java
@@ -228,7 +228,7 @@ public abstract class DocumentDetailsReport_Base
         }
 
         @Override
-        protected void addColumnDefintion(final Parameter _parameter,
+        protected void addColumnDefinition(final Parameter _parameter,
                                           final JasperReportBuilder _builder)
             throws EFapsException
         {
--- src/main/efaps/ESJP/org/efaps/esjp/accounting/transaction/Transaction_Base.java
+++ src/main/efaps/ESJP/org/efaps/esjp/accounting/transaction/Transaction_Base.java
@@ -1011,7 +1011,7 @@ public abstract class Transaction_Base
         }
 
         @Override
-        protected void addColumnDefintion(final Parameter _parameter,
+        protected void addColumnDefinition(final Parameter _parameter,
                                           final JasperReportBuilder _builder)
             throws EFapsException
         {
